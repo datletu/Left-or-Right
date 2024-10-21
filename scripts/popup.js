7c2dedc1a8612964680d0c4617e4a555dee8fa04
datletu <52278059+datletu@users.noreply.github.com>
2024-10-21 16:14:20 +0800

    .

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,10 +1,24 @@
 document.getElementById('checkUrl').addEventListener('click', async () => {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
     let url = tabs[0].url;
-    let newsWebsites = ["cnn.com", "bbc.com", "nytimes.com", "theguardian.com"];
-    let isNewsWebsite = newsWebsites.some(site => url.includes(site));
-    const notification = isNewsWebsite ? "This is a news website." : "This is not a news website.";
-    alert(notification);
-    
+    let newsWebsites = {"cnn.com":20, "bbc.com":0, "nytimes.com":0, "theguardian.com":0};
+    let source = "";
+    let leftLevel = 0;
+    let isNewsWebsite = false;
+    for (let website in newsWebsites) {
+      if (url.includes(website)) {
+        isNewsWebsite = true;
+        source = website;
+        leftLevel = newsWebsites[website];
+        break;
+      }
+    }
+    if (isNewsWebsite) {
+      alert(leftLevel);
+    }
+    else {
+      alert("This is not in our news websites database");
+    }
+   
 });
 });
